f77bf4f0a50346ccd544b2ef5912abba4eb8fff5
zealzel <zealzel@gmail.com>
2023-05-11 11:30:51 +0800

    1. diffdrive releated paramters  - wheel_separation from config/my_controllers.yaml  - enc_counts_per_rev from description/ros2_control.xacro 2. slam & nav parameters -> slam & nav both works fine now.  - slam: config/mapper_params_online_async.yaml  - nav: config/nav2_params.yaml

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -64,7 +64,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -77,7 +77,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
