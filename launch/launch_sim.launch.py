ead6966b880cffe480b35ac77dc3b0756746df96
zealzel <zealzel@gmail.com>
2023-08-14 12:40:42 +0800

    Update ros2_control.xacro

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -17,54 +17,87 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     world_arg = DeclareLaunchArgument(
-        "world", default_value="/home/zealzel/zbot2_ws/src/articubot_one/worlds/obstacles.world", description="Name of the map"
+        "world",
+        default_value="/home/zealzel/zbot2_ws/src/articubot_one/worlds/obstacles.world",
+        description="Name of the map",
     )
 
-    package_name='articubot_one' #<--- CHANGE ME
+    package_name = "articubot_one"  # <--- CHANGE ME
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true", "use_ros2_control": "true"}.items(),
     )
     joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "joystick.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux_params = os.path.join(
+        get_package_share_directory(package_name), "config", "twist_mux.yaml"
+    )
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {"use_sim_time": True}],
+        remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")],
+    )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(
+        get_package_share_directory(package_name), "config", "gazebo_params.yaml"
+    )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #                 launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-    #          )
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gazebo.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
+        }.items(),
+    )
 
     gzserver = ExecuteProcess(
         # cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', '--ros-args', '--params-file', gazebo_params_file],
-        cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', LaunchConfiguration('world')],
-        output='screen'
+        cmd=[
+            "gzserver",
+            "--verbose",
+            "-s",
+            "libgazebo_ros_init.so",
+            "-s",
+            "libgazebo_ros_factory.so",
+            LaunchConfiguration("world"),
+        ],
+        output="screen",
     )
 
-    gzclient = ExecuteProcess(
-        cmd=['gzclient', '--verbose'],
-        output='screen'
-    )
+    gzclient = ExecuteProcess(cmd=["gzclient", "--verbose"], output="screen")
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
-
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description", "-entity", "my_bot"],
+        output="screen",
+    )
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -106,17 +139,19 @@ def generate_launch_description():
     #     )
     # )
 
-    return LaunchDescription([
-        world_arg,
-        rsp,
-        joystick,
-        twist_mux,
-        # gazebo,
-        gzserver,
-        gzclient,
-        spawn_entity,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
-        # diff_drive_spawner,
-        # joint_broad_spawner
-    ])
+    return LaunchDescription(
+        [
+            world_arg,
+            rsp,
+            joystick,
+            twist_mux,
+            # gazebo,
+            gzserver,
+            gzclient,
+            spawn_entity,
+            delayed_diff_drive_spawner,
+            delayed_joint_broad_spawner,
+            # diff_drive_spawner,
+            # joint_broad_spawner
+        ]
+    )
