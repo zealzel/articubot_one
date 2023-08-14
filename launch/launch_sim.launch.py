56423a6445b33032312729be60e3e3297e0a984b
zealzel <zealzel@gmail.com>
2023-08-14 10:50:37 +0800

    Update launch_sim.launch.py. - Replace gazebo_ros launch file with gzserver & gzclient process. - To avoid diff_drive_spawner/joint_broad_spawner error due to controller_manager not loading issues

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,35 +2,31 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     package_name='articubot_one' #<--- CHANGE ME
-
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
-
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
-
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
@@ -42,11 +38,22 @@ def generate_launch_description():
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #                 launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+    #          )
+
+    gzserver = ExecuteProcess(
+        # cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', '--ros-args', '--params-file', gazebo_params_file],
+        cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', LaunchConfiguration('world')],
+        output='screen'
+    )
+
+    gzclient = ExecuteProcess(
+        cmd=['gzclient', '--verbose'],
+        output='screen'
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -67,32 +74,44 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=gzserver,
+            on_start=[diff_drive_spawner],
+        )
+    )
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=gzserver,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
     # delayed_diff_drive_spawner = RegisterEventHandler(
     #     event_handler=OnProcessExit(
     #         target_action=spawn_entity,
     #         on_exit=[diff_drive_spawner],
     #     )
     # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
+    # delayed_joint_broad_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[joint_broad_spawner],
+    #     )
+    # )
 
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         joystick,
         twist_mux,
-        gazebo,
+        # gazebo,
+        gzserver,
+        gzclient,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+        # diff_drive_spawner,
+        # joint_broad_spawner
     ])
