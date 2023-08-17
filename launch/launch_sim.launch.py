20b830edabf775b8f121267abf2917f966637986
zealzel <zealzel@gmail.com>
2023-08-17 13:35:38 +0800

    Solve spawner error by using TimerAction

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,20 +1,14 @@
 import os
-
-from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
-
 from launch.actions import (
-    ExecuteProcess,
-    LogInfo,
+    DeclareLaunchArgument,
     IncludeLaunchDescription,
-    RegisterEventHandler,
+    TimerAction,
 )
 from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-from launch.event_handlers import OnProcessStart, OnProcessIO, OnProcessExit
 
 
 def generate_launch_description():
@@ -44,7 +38,7 @@ def generate_launch_description():
                     get_package_share_directory(package_name),
                     "launch",
                     "joystick.launch.py",
-                )
+                ),
             ]
         ),
         launch_arguments={"use_sim_time": "true"}.items(),
@@ -79,50 +73,6 @@ def generate_launch_description():
         }.items(),
     )
 
-    gazebo_ros_gzserver = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("gazebo_ros"),
-                    "launch",
-                    "gzserver.launch.py",
-                )
-            ]
-        ),
-        launch_arguments={
-            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
-        }.items(),
-    )
-    gazebo_ros_gzclient = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("gazebo_ros"),
-                    "launch",
-                    "gzclient.launch.py",
-                )
-            ]
-        ),
-        launch_arguments={
-            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
-        }.items(),
-    )
-
-    gzserver = ExecuteProcess(
-        cmd=[
-            "gzserver",
-            "--verbose",
-            "-s",
-            "libgazebo_ros_init.so",
-            "-s",
-            "libgazebo_ros_factory.so",
-            LaunchConfiguration("world"),
-        ],
-        output="screen",
-    )
-
-    gzclient = ExecuteProcess(cmd=["gzclient", "--verbose"], output="screen")
-
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(
         package="gazebo_ros",
@@ -152,46 +102,12 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-    # delayed_diff_drive_spawner = (
-    #     RegisterEventHandler(
-    #         OnProcessIO(
-    #             target_action=gzserver,
-    #             on_stdout=lambda event: LogInfo(
-    #                 # msg='Spawn request says "{}"'.format(event.text.decode().strip())
-    #                 msg='Loading joint'
-    #             ),
-    #             on_start=[diff_drive_spawner],
-    #         )
-    #     ),
-    # )
-
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=gzserver,
-            on_start=[diff_drive_spawner],
-        )
+    spawner = LaunchDescription([
+        TimerAction(
+            period=4.0,
+            actions=[diff_drive_spawner, joint_broad_spawner],
+        )],
     )
-
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=gzserver,
-            on_start=[joint_broad_spawner],
-        )
-    )
-
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-
-    # delayed_joint_broad_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[joint_broad_spawner],
-    #     )
-    # )
 
     return LaunchDescription(
         [
@@ -202,14 +118,9 @@ def generate_launch_description():
             # joystick,
             twist_mux,
             gazebo,
-            # gzserver,
-            # gzclient,
-            # gazebo_ros_gzserver,
-            # gazebo_ros_gzclient,
             spawn_entity,
-            diff_drive_spawner,
-            joint_broad_spawner,
-            # delayed_diff_drive_spawner,
-            # delayed_joint_broad_spawner,
+            spawner,
+            # diff_drive_spawner,
+            # joint_broad_spawner,
         ]
     )
