ca8a0de96e4ce4323df2db41dbc5145d9a032a5e
zealzel <zealzel@gmail.com>
2023-08-17 11:49:40 +0800

    Update launch_sim.launch.py. Increate controller_manager spawner timeout frmo 5s to 10s

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,19 +3,23 @@ import os
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
-from launch.actions import ExecuteProcess
+from launch.actions import (
+    ExecuteProcess,
+    LogInfo,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+)
 from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
-from launch.event_handlers import OnProcessExit
+from launch.event_handlers import OnProcessStart, OnProcessIO, OnProcessExit
 
 
 def generate_launch_description():
+    x_arg = DeclareLaunchArgument("x", default_value="0", description="x position")
+    y_arg = DeclareLaunchArgument("y", default_value="0", description="y position")
     world_arg = DeclareLaunchArgument(
         "world",
         default_value="/home/zealzel/zbot2_ws/src/articubot_one/worlds/obstacles.world",
@@ -75,8 +79,36 @@ def generate_launch_description():
         }.items(),
     )
 
+    gazebo_ros_gzserver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gzserver.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
+        }.items(),
+    )
+    gazebo_ros_gzclient = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gzclient.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
+        }.items(),
+    )
+
     gzserver = ExecuteProcess(
-        # cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', '--ros-args', '--params-file', gazebo_params_file],
         cmd=[
             "gzserver",
             "--verbose",
@@ -95,14 +127,23 @@ def generate_launch_description():
     spawn_entity = Node(
         package="gazebo_ros",
         executable="spawn_entity.py",
-        arguments=["-topic", "robot_description", "-entity", "my_bot"],
+        arguments=[
+            "-topic",
+            "robot_description",
+            "-entity",
+            "my_bot",
+            "-x",
+            LaunchConfiguration("x"),
+            "-y",
+            LaunchConfiguration("y"),
+        ],
         output="screen",
     )
 
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont"],
+        arguments=["diff_cont", "--controller-manager-timeout", "10"],
     )
 
     joint_broad_spawner = Node(
@@ -110,6 +151,19 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
+
+    # delayed_diff_drive_spawner = (
+    #     RegisterEventHandler(
+    #         OnProcessIO(
+    #             target_action=gzserver,
+    #             on_stdout=lambda event: LogInfo(
+    #                 # msg='Spawn request says "{}"'.format(event.text.decode().strip())
+    #                 msg='Loading joint'
+    #             ),
+    #             on_start=[diff_drive_spawner],
+    #         )
+    #     ),
+    # )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
@@ -142,16 +196,20 @@ def generate_launch_description():
     return LaunchDescription(
         [
             world_arg,
+            x_arg,
+            y_arg,
             rsp,
-            joystick,
+            # joystick,
             twist_mux,
-            # gazebo,
-            gzserver,
-            gzclient,
+            gazebo,
+            # gzserver,
+            # gzclient,
+            # gazebo_ros_gzserver,
+            # gazebo_ros_gzclient,
             spawn_entity,
-            delayed_diff_drive_spawner,
-            delayed_joint_broad_spawner,
-            # diff_drive_spawner,
-            # joint_broad_spawner
+            diff_drive_spawner,
+            joint_broad_spawner,
+            # delayed_diff_drive_spawner,
+            # delayed_joint_broad_spawner,
         ]
     )
