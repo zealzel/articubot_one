3f9ac338520a8c5190b88ad5f755901c7c422bc7
Jason <u912538@gmail.com>
2024-07-12 15:15:54 +0800

    Make map server a stand alone launch file

--- launch/map_server.launch.py
+++ launch/map_server.launch.py
@@ -1,5 +1,3 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -29,35 +27,8 @@ def generate_launch_description():
     default_mask_path_sim = get_path(
         package_name, ["masks", "keepout_mask_turtlebot3_world.yaml"]
     )
-    default_params_file_path = get_path(
-        package_name, ["config", "nav2_params_keepout_multi.yaml"]
-    )
     costmap_filter_info_launch_path = get_path(
         package_name, ["launch", "costmap_filter_info.launch.py"]
-    )
-
-    namespace_arg = DeclareLaunchArgument(
-        name="namespace",
-        default_value="",
-        description="Namespace",
-    )
-    use_namespace_arg = DeclareLaunchArgument(
-        name="use_namespace",
-        default_value="false",
-        description="Enable use_sime_time to true",
-    )
-    use_sim_arg = DeclareLaunchArgument(
-        name="sim",
-        default_value="false",
-        description="Enable use_sime_time to true",
-    )
-    use_rviz_arg = DeclareLaunchArgument(
-        name="rviz", default_value="false", description="Run rviz"
-    )
-    rviz_config_arg = DeclareLaunchArgument(
-        "rviz_config",
-        default_value=get_path(package_name, ["rviz", "multi_nav2_default_view.rviz"]),
-        description=("Full path to the ROS2 rviz config file"),
     )
 
     map_arg = DeclareLaunchArgument(
@@ -85,13 +56,6 @@ def generate_launch_description():
         condition=IfCondition(LaunchConfiguration("sim")),
     )
 
-    params_arg = DeclareLaunchArgument(
-        "params_file",
-        default_value=default_params_file_path,
-        description=(
-            "Full path to the ROS2 parameters file to use for all launched nodes"
-        ),
-    )
     keepout_params_arg = DeclareLaunchArgument(
         "keepout_params_file",
         default_value=get_path(package_name, ["params", "keepout_params.yaml"]),
@@ -130,23 +94,6 @@ def generate_launch_description():
         ],
     )
 
-    nav_launch_dir = os.path.join(
-        get_package_share_directory(package_name), "launch", "nav2_bringup"
-    )
-    nav2_bringup = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(nav_launch_dir, "bringup_launch.py")
-        ),
-        launch_arguments={
-            "map": "",
-            "map_server": "False",
-            "namespace": LaunchConfiguration("namespace"),
-            "use_namespace": LaunchConfiguration("use_namespace"),
-            "use_sim_time": LaunchConfiguration("sim"),
-            "params_file": LaunchConfiguration("params_file"),
-        }.items(),
-    )
-
     # if not delayed, nav2_bringup will not able to launch controllers successfully
     costmap_filter_info_delayed = LaunchDescription(
         [
@@ -157,27 +104,15 @@ def generate_launch_description():
         ],
     )
 
-    rviz = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(nav_launch_dir, "rviz_launch.py")),
-        launch_arguments={
-            "use_sim_time": LaunchConfiguration("sim"),
-            "namespace": LaunchConfiguration("namespace"),
-            "use_namespace": LaunchConfiguration("use_namespace"),
-            "rviz_config": LaunchConfiguration("rviz_config"),
-            "log_level": "warn",
-        }.items(),
-        condition=IfCondition(LaunchConfiguration("rviz")),
-    )
-
     return LaunchDescription(
         [
-            namespace_arg,
-            use_namespace_arg,
-            use_sim_arg,
-            use_rviz_arg,
-            rviz_config_arg,
-            params_arg,
-            nav2_bringup,
-            rviz,
+            keepout_params_arg,
+            map_arg,
+            map_sim_arg,
+            mask_arg,
+            mask_sim_arg,
+            map_server,
+            map_server_lifecyle,
+            costmap_filter_info_delayed,
         ]
     )
