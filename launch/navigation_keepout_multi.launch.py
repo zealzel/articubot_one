54301ed74faff8dca457e6e28654d78484cf0be1
Jason <u912538@gmail.com>
2024-07-12 17:04:37 +0800

    Remove codes which have been moved to map_server.launch.py

--- launch/navigation_keepout_multi.launch.py
+++ launch/navigation_keepout_multi.launch.py
@@ -20,20 +20,8 @@ def get_path(package_name, subpaths):
 def generate_launch_description():
     package_name = "articubot_one"
 
-    default_map_path = get_path("fitrobot", ["maps", "office_res002_0523.yaml"])
-    default_map_path_sim = get_path(package_name, ["maps", "turtlebot3_world.yaml"])
-
-    default_mask_path = get_path(
-        "fitrobot", ["masks", "keepout_mask_office_res002_0523.yaml"]
-    )
-    default_mask_path_sim = get_path(
-        package_name, ["masks", "keepout_mask_turtlebot3_world.yaml"]
-    )
     default_params_file_path = get_path(
         package_name, ["config", "nav2_params_keepout_multi.yaml"]
-    )
-    costmap_filter_info_launch_path = get_path(
-        package_name, ["launch", "costmap_filter_info.launch.py"]
     )
 
     namespace_arg = DeclareLaunchArgument(
@@ -59,32 +47,6 @@ def generate_launch_description():
         default_value=get_path(package_name, ["rviz", "multi_nav2_default_view.rviz"]),
         description=("Full path to the ROS2 rviz config file"),
     )
-
-    map_arg = DeclareLaunchArgument(
-        name="map",
-        default_value=default_map_path,
-        description="Navigation map path",
-        condition=UnlessCondition(LaunchConfiguration("sim")),
-    )
-    map_sim_arg = DeclareLaunchArgument(
-        name="map",
-        default_value=default_map_path_sim,
-        description="Navigation map path",
-        condition=IfCondition(LaunchConfiguration("sim")),
-    )
-    mask_arg = DeclareLaunchArgument(
-        "mask",
-        default_value=default_mask_path,
-        description="mask file for keepout layer",
-        condition=UnlessCondition(LaunchConfiguration("sim")),
-    )
-    mask_sim_arg = DeclareLaunchArgument(
-        "mask",
-        default_value=default_mask_path_sim,
-        description="mask file for keepout layer",
-        condition=IfCondition(LaunchConfiguration("sim")),
-    )
-
     params_arg = DeclareLaunchArgument(
         "params_file",
         default_value=default_params_file_path,
@@ -92,44 +54,6 @@ def generate_launch_description():
             "Full path to the ROS2 parameters file to use for all launched nodes"
         ),
     )
-    keepout_params_arg = DeclareLaunchArgument(
-        "keepout_params_file",
-        default_value=get_path(package_name, ["params", "keepout_params.yaml"]),
-        description="params file for keepout layer",
-    )
-
-    costmap_filter_info = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(costmap_filter_info_launch_path),
-        launch_arguments={
-            "params_file": LaunchConfiguration("keepout_params_file"),
-            "mask": LaunchConfiguration("mask"),
-        }.items(),
-    )
-
-    map_server = Node(
-        package="nav2_map_server",
-        executable="map_server",
-        name="map_server",
-        output="screen",
-        parameters=[
-            {
-                "yaml_filename": default_map_path_sim,
-            },
-        ],
-        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
-    )
-    map_server_lifecyle = Node(
-        package="nav2_lifecycle_manager",
-        executable="lifecycle_manager",
-        name="lifecycle_manager_map_server",
-        output="screen",
-        parameters=[
-            {"use_sim_time": LaunchConfiguration("sim")},
-            {"autostart": True},
-            {"node_names": ["map_server"]},
-        ],
-    )
-
     nav_launch_dir = os.path.join(
         get_package_share_directory(package_name), "launch", "nav2_bringup"
     )
@@ -146,17 +70,6 @@ def generate_launch_description():
             "params_file": LaunchConfiguration("params_file"),
         }.items(),
     )
-
-    # if not delayed, nav2_bringup will not able to launch controllers successfully
-    costmap_filter_info_delayed = LaunchDescription(
-        [
-            TimerAction(
-                period=4.0,
-                actions=[costmap_filter_info],
-            )
-        ],
-    )
-
     rviz = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(nav_launch_dir, "rviz_launch.py")),
         launch_arguments={
